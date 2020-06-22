7ddc6928e2adf7c641ce7eae7d4b78108991f283
Maria Joao <mj.lazaro8@gmail.com>
2020-06-22 18:10:08 +0100

    Routes/controllers/views (missing)

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,20 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktail = Cocktail.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+
+    # NOT DONE YET
+    # respond_to do |format|
+    #   format.html
+    #   format.pdf { render pdf: generate_pdf(@client) }
+    # end
+  end 
+
+  def new
+    
+  end
+
 end
